ac6db1ecd23152b97c7786b99de9baf42656b0f3
Lachlan Scott <z5207471@ad.unsw.edu.au>
2024-08-07 17:04:18 +0200

    Remove sample status bar icon

--- main.ts
+++ main.ts
@@ -24,17 +24,6 @@ export default class ObsidianLexicon extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		
 		this.statusBarItemEl = this.addStatusBarItem();
 
 		this.updateStatusBar();
